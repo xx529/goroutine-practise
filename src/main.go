e9432b6fa3f3c3863b3d0443c5b004f280675329
Hang <64747715+xx529@users.noreply.github.com>
2023-10-08 23:24:28 +0800

    test

--- src/main.go
+++ src/main.go
@@ -1,13 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	//fmt.Println("total Passengers", Passengers)
-	//RunCheckDesign1()
-	//fmt.Println()
-	//RunCheckDesign2()
-	//fmt.Println()
-	//RunCheckDesign3()
-	//runTimeOut()
-	//runNotifyWait()
+	fmt.Println("total Passengers", Passengers)
+	RunCheckDesign1()
+
+	fmt.Println()
+	RunCheckDesign2()
+
+	fmt.Println()
+	RunCheckDesign3()
+
+	fmt.Println()
+	runTimeOut()
+
+	fmt.Println()
+	runNotifyWait()
+
+	fmt.Println()
 	runClosePattern()
 }
