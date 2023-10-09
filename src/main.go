da5df95c113d095103e341db2a727637756193f9
Hang <64747715+xx529@users.noreply.github.com>
2023-10-09 23:18:29 +0800

    add p shutdown

--- src/main.go
+++ src/main.go
@@ -3,21 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("total Passengers", Passengers)
-	RunCheckDesign1()
+	//fmt.Println("total Passengers", Passengers)
+	//RunCheckDesign1()
+	//
+	//fmt.Println()
+	//RunCheckDesign2()
+	//
+	//fmt.Println()
+	//RunCheckDesign3()
+	//
+	//fmt.Println()
+	//runTimeOut()
+	//
+	//fmt.Println()
+	//runNotifyWait()
+	//
+	//fmt.Println()
+	//runClosePattern()
 
 	fmt.Println()
-	RunCheckDesign2()
-
-	fmt.Println()
-	RunCheckDesign3()
-
-	fmt.Println()
-	runTimeOut()
-
-	fmt.Println()
-	runNotifyWait()
-
-	fmt.Println()
-	runClosePattern()
+	runPShutDown()
 }
---
+++ src/p_shutdown.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func runPShutDown() {
+	fmt.Println("test")
+}
