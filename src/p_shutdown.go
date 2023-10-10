41075a5ed762d8ec16313ed143dd5b359cc59e7a
Hang <64747715+xx529@users.noreply.github.com>
2023-10-10 23:41:14 +0800

    update p shutdown

--- src/p_shutdown.go
+++ src/p_shutdown.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type GeneralShutDowner interface {
+	ShutDown(duration time.Duration) error
+}
+
+type ShutDownFunc func(time.Duration) error
+
+func (f ShutDownFunc) ShutDown(duration time.Duration) error {
+	return f(duration)
+}
 
 func runPShutDown() {
 	fmt.Println("test")
