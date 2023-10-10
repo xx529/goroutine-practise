aac9dd74e6164e80a99e458066a8de76635781bc
Hang <64747715+xx529@users.noreply.github.com>
2023-10-11 00:11:17 +0800

    update p shutdown

--- src/p_shutdown.go
+++ src/p_shutdown.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -15,6 +17,45 @@ func (f ShutDownFunc) ShutDown(duration time.Duration) error {
 	return f(duration)
 }
 
+func ConcurrentShutdown(waitTimeout time.Duration, shutDowners ...GeneralShutDowner) error {
+	c := make(chan struct{})
+
+	go func() {
+		var wg sync.WaitGroup
+		for _, s := range shutDowners {
+			wg.Add(1)
+			go func(s GeneralShutDowner) {
+				defer wg.Done()
+				s.ShutDown(waitTimeout)
+			}(s)
+		}
+		wg.Wait()
+		c <- struct{}{}
+	}()
+
+	timer := time.NewTimer(waitTimeout)
+	defer timer.Stop()
+
+	select {
+	case <-c:
+		return nil
+	case <-timer.C:
+		return errors.New("time out")
+	}
+}
+
+func ShutDownMaker(t int) func(duration time.Duration) error {
+	return func(duration time.Duration) error {
+		time.Sleep(time.Second * time.Duration(t))
+		return nil
+	}
+}
+
 func runPShutDown() {
-	fmt.Println("test")
+	f1 := ShutDownFunc(ShutDownMaker(3))
+	f2 := ShutDownFunc(ShutDownMaker(4))
+	f3 := ShutDownFunc(ShutDownMaker(5))
+
+	r := ConcurrentShutdown(time.Second*1, f1, f2, f3)
+	fmt.Println(r)
 }
