19c61b9bc194918da84303896326b63907f89bf1
Hang <64747715+xx529@users.noreply.github.com>
2023-10-19 23:22:29 +0800

    update p shutdown

--- src/main.go
+++ src/main.go
@@ -5,23 +5,23 @@ import (
 )
 
 func main() {
-	//fmt.Println("total Passengers", Passengers)
-	//RunCheckDesign1()
-	//
-	//fmt.Println()
-	//RunCheckDesign2()
-	//
-	//fmt.Println()
-	//RunCheckDesign3()
-	//
-	//fmt.Println()
-	//runTimeOut()
-	//
-	//fmt.Println()
-	//runNotifyWait()
-	//
-	//fmt.Println()
-	//runClosePattern()
+	fmt.Println("total Passengers", Passengers)
+	RunCheckDesign1()
+
+	fmt.Println()
+	RunCheckDesign2()
+
+	fmt.Println()
+	RunCheckDesign3()
+
+	fmt.Println()
+	runTimeOut()
+
+	fmt.Println()
+	runNotifyWait()
+
+	fmt.Println()
+	runClosePattern()
 
 	fmt.Println()
 	runPShutDown()
